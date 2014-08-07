6d5fabbdb3b2f9e81ad75c93c0b238fa0a6e14fa
François Revol <revol@free.fr>
2014-08-07 14:18:14 +0200

    Replace BeIDE header by the Pe version
    We won't be building for BeIDE anyway...

--- MWEditorAddOns/MTextAddOn.h
+++ MWEditorAddOns/MTextAddOn.h
@@ -1,3 +1,4 @@
+// [zooey, 2005]: made MTextAddon really an abstract class
 //==================================================================
 //	MTextAddOn.h
 //	Copyright 1996  Metrowerks Corporation, All Rights Reserved.
@@ -5,45 +6,40 @@
 //	This is a proxy class used by Editor add_ons.  It does not inherit from BView
 //	but provides an abstract interface to a text engine.
  
-#ifndef _MTEXTADDON_H
-#define _MTEXTADDON_H
+#ifndef MTEXTADDON_H
+#define MTEXTADDON_H
 
-#include <SupportKit.h>
+//#include "HLibHekkel.h"
 
-class MIDETextView;
 class BWindow;
 struct entry_ref;
 
-
-class MTextAddOn
+class /*IMPEXP_LIBHEKKEL*/ MTextAddOn
 {
 public:
-								MTextAddOn(
-									MIDETextView&	inTextView);
 	virtual						~MTextAddOn();
-	virtual	const char*			Text();
-	virtual	int32				TextLength() const;
-	virtual	void				GetSelection(
-									int32 *start, 
-									int32 *end) const;
-	virtual	void				Select(
-									int32 newStart, 
-									int32 newEnd);
-	virtual void				Delete();
-	virtual void				Insert(
-									const char* inText);
-	virtual void				Insert(
-									const char* text, 
-									int32 length);
+	virtual	const char*			Text() = 0;
+	virtual	int32				TextLength() const = 0;
+	virtual	void				GetSelection(int32 *start, int32 *end) const = 0;
+	virtual	void				Select(int32 newStart, int32 newEnd) = 0;
+	virtual void				Delete() = 0;
+	virtual void				Insert(const char* inText) = 0;
+	virtual void				Insert(const char* text, int32 length) = 0;
 
-	virtual	BWindow*			Window();
-	virtual status_t			GetRef(
-									entry_ref&	outRef);
-	virtual bool				IsEditable();
-	
-private:
-
-	MIDETextView&				fText;
+	virtual	BWindow*			Window() = 0;
+	virtual status_t			GetRef(entry_ref& outRef) = 0;
 };
+
+#if !__INTEL__
+#pragma export on
+#endif
+extern "C" {
+long perform_edit(MTextAddOn *addon);
+}
+#if !__INTEL__
+#pragma export reset
+#endif
+
+typedef long (*perform_edit_func)(MTextAddOn *addon);
 
 #endif
